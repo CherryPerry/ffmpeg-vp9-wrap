143bfff42d7f61287569dd0cc7f53769bf4abd31
Cherry Perry <ya.cherryperry@yandex.ru>
2015-06-22 14:26:20 +0300

    OOP timings realization

---
+++ VpxEncodeOOP/Args/TimingFileArg.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using Vp9Encode.Args.Attributes;
+
+namespace Vp9Encode.Args
+{
+  [InputSequence("-t")]
+  public class TimingFileArg : Arg
+  {
+    public override int Priority
+    {
+      get { return PRIORITY_LOW; }
+    }
+
+    public TimingFileArg(string file)
+      : base(file)
+    {
+      if (String.IsNullOrWhiteSpace(file))
+        throw new ArgumentException();
+      FileInfo fileInfo = new FileInfo(file);
+      if (!fileInfo.Exists)
+        throw new ArgumentException();
+    }
+
+    public override StringBuilder ApplyArg(StringBuilder args, Arg.ApplyTo to)
+    {
+      return args;
+    }
+  }
+}
---
+++ VpxEncodeOOP/Args/TimingIndexArg.cs
@@ -0,0 +1,25 @@
+﻿using System.Text;
+using Vp9Encode.Args.Attributes;
+
+namespace Vp9Encode.Args
+{
+  [InputSequence("-ti")]
+  public class TimingIndexArg : Arg
+  {
+    public override int Priority
+    {
+      get { return PRIORITY_LOW; }
+    }
+
+    public TimingIndexArg(string index)
+      : base(index)
+    {
+      ushort.Parse(index);
+    }
+
+    public override StringBuilder ApplyArg(StringBuilder args, Arg.ApplyTo to)
+    {
+      return args;
+    }
+  }
+}
